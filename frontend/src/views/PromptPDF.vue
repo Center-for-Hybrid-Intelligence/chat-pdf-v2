a99d1e1d3965ce4e27fa3f47702a51693f74451b
Baptiste Rabecq <baptiste.rabecq@orange.fr>
2023-06-14 13:51:16 +0200

    Address changes

--- frontend/src/views/PromptPDF.vue
+++ frontend/src/views/PromptPDF.vue
@@ -61,7 +61,7 @@ export default {
       };
       loading_answer.value = true;
       has_answer.value = true;
-      axios.post(baseUrl.value + '/ask-query/', request_data, {
+      axios.post(baseUrl.value + '/api/ask-query/', request_data, {
               headers: {
                 "Content-Type": "application/json",
               },
@@ -104,7 +104,7 @@ export default {
 
     const eraseEntries = async () => {
       try {
-        await axios.get(baseUrl.value + '/erase-all/');
+        await axios.get(baseUrl.value + '/api/erase-all/');
         console.log('HTTP request sent!');
       } catch (error) {
         console.error('Failed to send HTTP request:', error);
