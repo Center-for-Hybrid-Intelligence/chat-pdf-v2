c35a04aece2de6ae7fdb727bf574ad65a1fa4d8e
Baptiste Rabecq <baptiste.rabecq@orange.fr>
2023-06-16 12:07:39 +0200

    Add authservice

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,5 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import auth from './modules/auth';
 
-Vue.use(Vuex);
-
-export default new Vuex.Store({
+export default({
   modules: {
-    auth
   }
 });
