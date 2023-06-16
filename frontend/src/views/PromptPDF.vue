7323783652b893d5c5e92ceb66590654c30fc16d
Pieterwerff <pieterwerff1@gmail.com>
2023-06-16 12:46:29 +0200

    disabled button

--- frontend/src/views/PromptPDF.vue
+++ frontend/src/views/PromptPDF.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="flex flex-col items-center justify-center w-screen mt-32"
   >
-    <div class="flex flex-col gap-8 items-center w-max">
+    <div class="flex flex-col gap-8 items-center">
 
       <label class="block text-7xl font-bold text-gray-700">
         Write your question
       </label>
       <TextArea @input="onChange" class="h-64"></TextArea>
       <div class="flex flex-row gap-4">
-        <Button class="button primary" @click="sendRequest">
+        <Button :isDisabled="loading" class="button primary" @click="sendRequest">
           <template #right>Submit</template>
         </Button>
+        {{loading}}
       </div>
-      <div v-for="(question, index) in questionList.questions" :key="index">
+      <div v-for="(question, index) in questionList.questions.reverse()" :key="index">
         <div class="flex flex-col gap-4 items-center border-2 border-green-600 max-w-screen-h4 h8:max-w-2xl break-words rounded-s p-6" v-if="answer.result" >
-          <div class="block text-xl text-start font-medium text-gray-700">
+          <div class="block text-xs text-start font-medium text-gray-700">
             <h1 class="heading2"> {{ question }} </h1>
              {{questionList.answers[index]}}
             <div class="text-blue-950">
               <h1 class="heading2"> Source documents </h1>
-              {{ answer.source_documents }}</div>
+              <div
+              style="white-space: pre-wrap"
+              >
+                {{ answer.source_documents }}
+              </div></div>
           </div>
         </div>
       </div>
@@ -50,11 +55,14 @@ export default {
       sourceDocuments: [],
     });
 
+    const loading = ref(false);
+
     const onChange = (value) => {
       query.value = value
     }
 
     const sendRequest = async () => {
+      loading.value = true;
       questionList.questions.push(query.value);
       const request_data = {
         query: query.value,
@@ -72,9 +80,11 @@ export default {
             questionList.answers.push(answer.value.result);
             questionList.sourceDocuments.push(answer.value.source_documents);
             console.log(response.data);
+            loading.value = false;
           })
           .catch((error) => {
             console.log(error);
+            loading.value = false;
           });
     }
 
@@ -119,7 +129,8 @@ export default {
       query,
       sendRequest,
       onChange,
-      questionList
+      questionList,
+      loading
     }
   }
 }
