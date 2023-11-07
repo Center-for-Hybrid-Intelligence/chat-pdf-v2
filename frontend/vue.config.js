1799fad7a27d16136483675f814b180f6e5005a2
Max Roald Eckardt <mr.eckardt@gmail.com>
2023-11-07 22:44:16 +0100

    hack for dev server

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -9,6 +9,7 @@ const basePath = '/chat-pdf'
 module.exports = {
   devServer: {
     port: 3051,
+    disableHostCheck: true
   },
   publicPath: process.env.NODE_ENV === 'production' ? basePath : '/',
   configureWebpack: {
